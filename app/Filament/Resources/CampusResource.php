7f671ac7f6eed101dce9746f134be62c61a3cc94
199ocero <199ocero@gmail.com>
2023-01-27 22:19:01 +0800

    add campus

---
+++ app/Filament/Resources/CampusResource.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CampusResource\Pages;
+use App\Filament\Resources\CampusResource\RelationManagers;
+use App\Models\Campus;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CampusResource extends Resource
+{
+    protected static ?string $model = Campus::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationGroup = 'Data Management';
+    protected static ?int $navigationSort = 2;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->unique()
+                    ->string(),
+                Forms\Components\Select::make('district_id')
+                    ->required()
+                    ->relationship('district', 'name'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Campus Name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('district.name')
+                    ->label('District Name')
+                    ->sortable()
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCampuses::route('/'),
+            'create' => Pages\CreateCampus::route('/create'),
+            'edit' => Pages\EditCampus::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/Campus.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Campus extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'district_id',
+        'name',
+    ];
+
+    public function district()
+    {
+        return $this->belongsTo(District::class);
+    }
+}
