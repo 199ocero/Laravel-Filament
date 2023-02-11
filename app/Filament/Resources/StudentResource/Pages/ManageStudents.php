81f6ba35c5040a9172ca8cf944c955dcb732103c
jaocero <jay@redcircleglobal.com>
2023-02-11 10:49:27 +0800

    remove import plugin

--- app/Filament/Resources/StudentResource/Pages/ManageStudents.php
+++ app/Filament/Resources/StudentResource/Pages/ManageStudents.php
@@ -43,49 +43,6 @@ class ManageStudents extends ManageRecords
 
                     return $data;
                 }),
-            ImportAction::make()
-                ->handleBlankRows(true)
-                ->uniqueField('email')
-                ->uniqueField('lrn')
-                ->fields([
-                    ImportField::make('lrn')
-                        ->label('LRN')
-                        ->required(),
-                    ImportField::make('email')
-                        ->label('Email')
-                        ->required(),
-                    ImportField::make('first_name')
-                        ->label('First Name')
-                        ->required(),
-                    ImportField::make('middle_name')
-                        ->label('Middle Name')
-                        ->required(),
-                    ImportField::make('last_name')
-                        ->label('Last Name')
-                        ->required(),
-                    ImportField::make('suffix')
-                        ->label('Suffix'),
-                    ImportField::make('birthday')
-                        ->label('Birthday')
-                        ->required(),
-                ], columns: 3)
-                ->handleRecordCreation(function ($data) {
-                    if (!Role::where('name', 'student')->exists()) {
-                        Role::create(['name' => 'student']);
-                    }
-
-                    $user = new User();
-                    $user->name = $data['first_name'] . ' ' . $data['middle_name'] . ' ' . $data['last_name'];
-                    $user->email = $data['email'];
-                    $user->password = Hash::make($data['birthday']);
-                    $user->save();
-
-                    $user->assignRole('student');
-
-                    $data['user_id'] = $user->id;
-
-                    return Student::create($data);
-                })
         ];
     }
 }
